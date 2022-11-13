e6d50fa2ab38be87616b7592bae3395d1d0d3ecc
danielfcollier <danielfcollier@gmail.com>
2022-11-13 20:49:58 -0300

    Add new router controllers.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ResetController } from './reset/reset.controller';
+import { BalanceController } from './balance/balance.controller';
+import { EventController } from './event/event.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    ResetController,
+    BalanceController,
+    EventController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
